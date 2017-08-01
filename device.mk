31818ea7cb9d22e2abacb68d4447d3f24b875579
DigiGoon <kagatharatarang@gmail.com>
2017-08-01 09:30:06 +0530

    Enable Doze

--- device.mk
+++ device.mk
@@ -97,6 +97,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     bt_vendor.conf
 
+# Doze
+PRODUCT_PACKAGES += \
+    HisiDoze
+
 # Fingerprint sensor
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:system/etc/permissions/android.hardware.fingerprint.xml
